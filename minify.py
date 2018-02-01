2ffffe4f2db6e9642032c16a792b7c439f14a50a
igorzakhar <igorzakhar@gmail.com>
2018-02-01 18:53:44 +0300

    Rename some variables

--- minify.py
+++ minify.py
@@ -3,26 +3,25 @@ import os
 import requests
 
 
-def get_list_of_files(dir):
-    return os.listdir(dir)
+def get_list_of_files(directory):
+    return os.listdir(directory)
 
 
-def open_file(file):
-    with open(file, 'rb') as file:
+def open_file(file_path):
+    with open(file_path, 'rb') as file:
         return file.read()
 
 
-def save_file(file, response):
-    with open(file, 'w') as file:
+def save_file(file_path, response):
+    with open(file_path, 'w') as file:
         file.write(response)
 
 
-def minify(url, file):
-    # path = '{}{}'.format(dir, file)
-    fp = open_file(file)
-    data = {'input': fp}
-    response = requests.post(url, data=data)
-    save_file(file, response.text)
+def minify(url, file_path):
+    file_content = open_file(file_path)
+    payload = {'input': file_content}
+    response = requests.post(url, data=payload)
+    save_file(file_path, response.text)
 
 
 if __name__ == '__main__':
@@ -33,8 +32,8 @@ if __name__ == '__main__':
     list_js_files = list(map(lambda f: dir_js + f, os.listdir(dir_js)))
     list_css_files = list(map(lambda f: dir_css + f, os.listdir(dir_css)))
     list_all_files = [*list_js_files, *list_css_files]
-    for file in list_all_files:
-        if file.endswith('.css'):
-            minify(api_css, file)
+    for filename in list_all_files:
+        if filename.endswith('.css'):
+            minify(api_css, filename)
         else:
-            minify(api_js, file)
+            minify(api_js, filename)
